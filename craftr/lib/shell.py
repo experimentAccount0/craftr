e9bae66b3180a4acb03b6428800518996b1ddf35
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-08-31 10:15:43 +0200

    add craftr/lib/shell with proper split() and quote() methods

---
+++ craftr/lib/shell.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2017 Niklas Rosenstein
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import re
+import shlex
+
+
+class safe(str):
+  pass
+
+
+
+def split(s):
+  """
+  Enhanced implementation of #shlex.split().
+  """
+
+  result = shlex.split(s, posix=(os.name != 'nt'))
+  if os.name == 'nt':
+    # With posix=False, shlex.split() will not interpret \ as the
+    # escape character (which is what we need on windows), but it
+    # will also not eliminate quotes, which is what we need to do
+    # here now.
+    quotes = '\"\''
+    result = [x[1:-1] if (x and x[0] in quotes and x[-1] in quotes) else x for x in result]
+  return result
+
+
+def quote(s, for_ninja=False):
+  """
+  Enhanced implementation of #shlex.quote() which works correctly on Windows.
+  """
+
+  if isinstance(s, safe):
+    return s
+  if os.name == 'nt' and os.sep == '\\':
+    # Custom quoting on Windows using double-quotes.
+    s = s.replace('"', '\\"')
+    if re.search('\s', s) or any(c in s for c in '<>'):
+      s = '"' + s + '"'
+  else:
+    s = shlex.quote(s)
+  if for_ninja:
+    # Fix escaped $ variables on Unix, see issue craftr-build/craftr#30
+    s = re.sub(r"'(\$\w+)'", r'\1', s)
+  return s
+
+
+
+def join(args):
+  return ' '.join(map(quote, args))
